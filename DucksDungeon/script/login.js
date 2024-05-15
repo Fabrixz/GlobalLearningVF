eba20573ea2a7fd7409b63865d3cbef0cadc3015
Fabrixz <122574416+Fabrixz@users.noreply.github.com>
2024-05-15 19:49:57 -0300

    third commit
    third commit

--- DucksDungeon/script/login.js
+++ DucksDungeon/script/login.js
@@ -18,3 +18,13 @@ let nombreGuardado = localStorage.getItem('nombreUsuario');
 if (nombreGuardado) {
     console.log('Nombre guardado: ' + nombreGuardado);
 }
+function guardarNombre() {
+    const nombre = document.getElementById('nombre').value;
+    if (nombre) {
+        localStorage.setItem('nombreUsuario', nombre); // Guarda el nombre en localStorage
+        window.location.href = '/DucksDungeon/pages/menu.html'; // Redirige al usuario a index.html
+    } else {
+        alert('Por favor, ingresa tu nombre antes de enviar.');
+    }
+}
+
