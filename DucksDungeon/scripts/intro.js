406f600496b9bd288606db44d153abbdf4b9cd06
valejandro <leguizamonalejandroisfa@gmail.com>
2024-05-16 17:48:44 -0300

    Menu, Intro : JS, HTML y CSS updated

--- DucksDungeon/scripts/intro.js
+++ DucksDungeon/scripts/intro.js
@@ -1,15 +1,48 @@
 document.addEventListener('DOMContentLoaded', () => {
-    // Obtener el parámetro de color de fondo de la URL
+
     const urlParams = new URLSearchParams(window.location.search);
-    const bgColor = urlParams.get('bg-color');
+    const encodedBgColor = urlParams.get('bg-color');
+    const encodedMadrePato = urlParams.get('madre-pato');
+    const encodedBodyColor = urlParams.get('body-color');
+    const encodedGoBackBtn = urlParams.get('go-back-btn');
 
-    // Seleccionar el elemento con la clase .container
+
+    const bgColor = decodeURIComponent(encodedBgColor);
+    const madrePato = "/" + decodeURIComponent(encodedMadrePato);
+    const bodyColor = decodeURIComponent(encodedBodyColor);
+    const goBackBtn = "/" + decodeURIComponent(encodedGoBackBtn);
+
+    console.log("Madre pato svg path: ", madrePato);
+    console.log("Container background color: ", bgColor);
+    console.log("Body background color: ", bodyColor)
+    console.log("Header background color: ", bodyColor)
+    console.log("Go back button path: ", goBackBtn)
+
+    
+
     const containerElement = document.querySelector('.container');
+    const imgElement = document.getElementById('egg');
+    const bodyElement = document.querySelector('body');
+    const headerIntro = document.querySelector('header');
+    const goBackElement = document.getElementById('go-back'); 
 
-    // Aplicar el color de fondo al elemento .container
+    if (imgElement) {
+        imgElement.src = madrePato;
+    } else {
+        console.error('No se ha encontrado al elemento con el id EGG');
+    }
+
     if (containerElement) {
         containerElement.style.backgroundColor = bgColor;
     } else {
-        console.error('No se encontró el elemento con la clase .container');
+        console.error('No se ha encontrado el elemento con la clase .container');
+    }
+
+    if (bodyElement && headerIntro && goBackElement) {
+        bodyElement.style.backgroundColor = bodyColor;
+        headerIntro.style.backgroundColor = bodyColor;
+        goBackElement.src = goBackBtn;
+    } else {
+        console.error('No se ha encontrado el elemento body')
     }
 });
--- DucksDungeon/scripts/menu.js
+++ DucksDungeon/scripts/menu.js
@@ -1,3 +1,7 @@
-function redirectToIntro(bgColor) {
-    window.location.href = `intro.html?bg-color=${bgColor}`;
+function redirectToIntro(bgColor, madrePato, bodyColor, goBackBtn) {
+    const encodedBgColor = encodeURIComponent(bgColor); // Codificamos el color porque el # no es bien interpretado en la barra de búsqueda. Luego lo decodificamos :)
+    const encodedMadrePato = encodeURIComponent(madrePato);
+    const encodedBodyColor = encodeURIComponent(bodyColor);
+    const encodedGoBackBtn = encodeURIComponent(goBackBtn);
+    window.location.href = `intro.html?bg-color=${encodedBgColor}&madre-pato=${encodedMadrePato}&body-color=${encodedBodyColor}&go-back-btn=${encodedGoBackBtn}`;
 }
