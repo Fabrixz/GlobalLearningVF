f84d956e00909e0a274ebc0d94fc6fb0a263416a
valejandro <leguizamonalejandroisfa@gmail.com>
2024-05-17 08:29:34 -0300

    index input username updated

--- DucksDungeon/scripts/index.js
+++ DucksDungeon/scripts/index.js
@@ -1,5 +1,10 @@
-function saveName() {
+function redirectToMenu(event) {
+    event.preventDefault();
     const username = document.getElementById('nameInput').value;
-    localStorage.setItem('username', username)
-    console.log(localStorage.getItem('username'))
+    if (username) {
+        localStorage.setItem('username', username);
+        window.location.href = 'menu.html';
+    } else {
+        alert("Necesitas ingresar tu nombre para poder continua.")
+    }
 }
