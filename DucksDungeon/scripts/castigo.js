94ab74bb2cf11cec444b825a6f807edbb559ded2
Fabrixz <122574416+Fabrixz@users.noreply.github.com>
2024-05-18 20:38:30 -0300

    First punnish
    Punnish game

---
+++ DucksDungeon/scripts/castigo.js
@@ -0,0 +1,28 @@
+let clics = 0;
+let tiempo = 25;
+let intervalo;
+
+document.addEventListener("DOMContentLoaded", function() {
+    intervalo = setInterval(actualizarTiempo, 1000);
+});
+
+function contarClics() {
+    if (clics < 160 && tiempo > 0) {
+        clics++;
+        document.getElementById('contadorClics').innerText = clics;
+    }
+}
+
+function actualizarTiempo() {
+    if (tiempo > 0) {
+        tiempo--;
+        document.getElementById('contadorTiempo').innerText = tiempo;
+    } else {
+        clearInterval(intervalo);
+        if (clics < 160) {
+            document.getElementById('susto').style.display = 'block';
+            // Aquí puedes reiniciar el juego o hacer otra acción
+        }
+    }
+}
+
