1b7773244d2b170c15bee85909e5dfc26182e6f1
Fabrixz <122574416+Fabrixz@users.noreply.github.com>
2024-05-22 19:07:30 -0300

    Test with a friend
    Mole game fixed

--- DucksDungeon/scripts/mole.js
+++ DucksDungeon/scripts/mole.js
@@ -28,15 +28,15 @@ playBtn.addEventListener("click", () => {
     hole = holes[arrayNo];
 
     let image = document.createElement("img");
-    image.setAttribute("src", "https://previews.dropbox.com/p/thumb/AA8m-hEZXJI_bVZ712hEmOsO0lr1Q-0YPNVQdM1bSv76OP1cyZs-T0VeigdLEezp7pjNl9jSdmUQhVoZdC_tkGvsP0QRH2Ru-IctZj_3O-URgAFasZqHeiLAQbHykAYYWEIIuYtecoLSKbxPx7KMUNplBM6Kp4rFqAF41IeQKG7wkNjaoF192bgyVaq2WUysHKSSr6UE4Y4JpSGXfFE3dg-2rs3XcAk7noO7BOYOd5fbX3E8zc7cLuJ5yGVbbkX6v7D--_63fghCjbgwKL_QzHk_G0v6J2l2F8k1OQ2ESQ72YJodx8Yv_u-ZiId-3VTWbYZLHDqpSE0Wh7GbmSrNZXAE4ZdExd-iHw-jMcJ3_wBaOg/p.png?fv_content=true&size_mode=5");
+    image.setAttribute("src", "https://cdn3.iconfinder.com/data/icons/spring-2-1/30/Worm-64.png");
     image.setAttribute("class", "mole");
     hole.appendChild(image);
 
     setTimeout(() => {
       hole.removeChild(image);
-    }, 200); // Cambia este valor para aumentar la frecuencia (por ejemplo, 200 ms)
+    }, 400); // Cambia este valor para aumentar la frecuencia (por ejemplo, 200 ms)
 
-  }, 300); // Cambia este valor para aumentar la frecuencia (por ejemplo, 300 ms)
+  }, 600); // Cambia este valor para aumentar la frecuencia (por ejemplo, 300 ms)
 
   window.addEventListener("click", (e) => {
     if (e.target === hole) score.innerText = ++points;
@@ -45,7 +45,7 @@ playBtn.addEventListener("click", () => {
   stopBtn.addEventListener("click", () => {
     clearInterval(startGame);
     stopBtn.style.display = "none";
-    playBtn.style.display = "inline-block";
+    playBtn.style.display = "inline-block"
     score.innerText = 0;
   });
 });
